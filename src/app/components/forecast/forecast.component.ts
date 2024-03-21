0a94edce491cbf8d54a2d9f26130bde904b0aa48
Rahul Panvalkar <rahulpanvalkar59@gmail.com>
2024-03-22 00:08:41 +0530

    Add a forecast screen

--- src/app/components/forecast/forecast.component.ts
+++ src/app/components/forecast/forecast.component.ts
@@ -30,6 +30,11 @@ export class ForecastComponent {
   maxTemp_f!: number;
   temperature_f!:number;
 
+  dataCard: any[] = []; 
+  hourlyDataCard: any[] = [];
+  currentIndex: number = 0;
+  cardsToShow: number = 10;
+
   constructor(private weatherService: WeatherService,private toggleService: ToggleService){  }
   toggleValueSubscription!: Subscription;
 
@@ -41,6 +46,8 @@ export class ForecastComponent {
       this.inCelsius = !value; 
     });
 
+    this.setData();
+    
   }
 
   ngOnDestroy(): void {
@@ -48,70 +55,42 @@ export class ForecastComponent {
     this.toggleValueSubscription.unsubscribe();
   }
 
-  onEnterKeyPressed(event: any) {
-    console.log("inside onEnterKeyPressed..")
-    const cityName = (event.target as HTMLInputElement)?.value;
-    console.log(cityName);
-    if (cityName) {
+  setData(){
+    const dataObj: { weather: WeatherData, forecast: WeatherForecast } = this.weatherService.getPreviousData(); 
+    console.log("setData >> dataObj :: ",dataObj);
 
-      this.weatherService.getWeatherData(cityName)
-        .subscribe({
-          next: (weatherResponse: WeatherData) => {
-            console.log("weather response :: ", weatherResponse);
-
-            this.weatherService.getForcastData(cityName)
-              .subscribe({
-                next: (forecastResponse: any) => {
-                  console.log("forecast response :: ", forecastResponse);
-                  this.updateData(weatherResponse, forecastResponse);
-                },
-
-                error: (errorCode: string) => {
-                  console.error("Error fetching forecast data: ", errorCode);
-                  if(errorCode == '400'){
-                    alert("Invalid city! please check city name");
-                  }
-                  else if(errorCode == '502'){
-                    alert("Something went wrong! please try again");
-                  }
-                }
-              });
-
-          },
-
-          error: (errorCode: string) => {
-            console.error("Error fetching weather data: ", errorCode);
-            if(errorCode == '400'){
-              alert("Invalid city! please check city name");
-            }
-          }
-        });
+    if (!dataObj || !dataObj.weather || !dataObj.forecast) {
+      console.error("dataObj or its properties are undefined.");
+      return;
     }
-    
 
+    let weatherData = dataObj.weather;
+    let forecastData = dataObj.forecast;
+    
+    console.log("updateView >> weatherData :: ", weatherData);
+    console.log("updateView >> forcastData :: ", forecastData);
+    console.log('updateView >> inCelsius :: ', this.inCelsius);
+    
+    this.dataCard = forecastData.forecast.forecastday;
+    console.log(this.dataCard);
+    this.hourlyDataCard = forecastData.forecast.forecastday[0].hour;
+    
+  }
+  
+  getNextCards(): void {
+    if (this.currentIndex + this.cardsToShow < this.hourlyDataCard.length) {
+      this.currentIndex += this.cardsToShow;
+    }
   }
 
-  updateData(weatherData: WeatherData, forcastData: WeatherForecast) {
-    console.log("weatherData :: ", weatherData);
-    console.log("forcastData :: ", forcastData);
-    console.log('inCelsius :: ', this.inCelsius);
+  getPreviousCards(): void {
+    if (this.currentIndex - this.cardsToShow >= 0) {
+      this.currentIndex -= this.cardsToShow;
+    }
+  }
 
-    this.iconURL = weatherData.current.condition.icon;
-    this.currentDateTime = weatherData.location.localtime;
-    this.lastUpdatedTime = weatherData.current.last_updated;
-    this.weatherStatus = weatherData.current.condition.text;
-    this.cityName = weatherData.location.name;
-    this.humidity = weatherData.current.humidity;
-    this.wind = weatherData.current.wind_kph;
-
-    this.temperature_c = weatherData.current.temp_c;
-    this.minTemp_c = forcastData.forecast.forecastday[0].day.mintemp_c;
-    this.maxTemp_c = forcastData.forecast.forecastday[0].day.maxtemp_c;
-
-    this.temperature_f = weatherData.current.temp_f;
-    this.minTemp_f = forcastData.forecast.forecastday[0].day.mintemp_f;
-    this.maxTemp_f = forcastData.forecast.forecastday[0].day.maxtemp_f;
-    
+  getVisibleCards(): any[] {
+    return this.hourlyDataCard.slice(this.currentIndex, this.currentIndex + this.cardsToShow);
   }
 }
 
